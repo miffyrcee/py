41b2339cbb1e73c8e019203445cd0699eeb4f333
miffyrcee <miffyrcee@outlook.com>
2019-06-08 13:03:00 +0800

    update

---
+++ activity.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 miffyrcee <miffyrcee@localhost.localdomain>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import numpy as np
+import numpy as np
+
+import itertools
+a = itertools.combinations(([2,3]+[3,6,7,7]),4)
+
+for i 
+    print()[2,3]+[3,6,7,7]
+print([2,3]+[3,6,7,7])
+
---
+++ disjoint.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 miffyrcee <miffyrcee@localhost.localdomain>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import itertools
+from collections import deque
+import collections
+
+
