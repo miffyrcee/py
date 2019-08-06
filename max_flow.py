5dd708ef8c1e2556b3ae4699025c97e9aaf47a7b
miffyrcee <miffyrcee@outlook.com>
2019-08-06 21:36:03 +0800

    delete

--- max_flow.py
+++ max_flow.py
@@ -1,9 +1,11 @@
 import math
 import string
 import time
-from random import choices
+from random import choice, choices
 
 import numpy as np
+from numpy.linalg import *
+from numpy.ma import *
 
 n = 5
 w = choices(string.digits, k=n * n)
@@ -11,4 +13,3 @@ w = np.reshape(w, (n, n)).astype(float)
 w = np.where(w == 0, np.inf, w)
 for i, j in zip(range(n), range(n)):
     w[i][j] = 0
-print(w)
